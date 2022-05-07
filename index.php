c79cb6320c349c5d96063400fa7c2e72dc2c3558
George <gera.sukhomlin@gmail.com>
2022-05-07 21:35:39 +0200

    Created header and footer

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
-<?
-require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
-$APPLICATION->SetTitle("Главная");
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+</head>
+<body>
+
+
+
+<?php
+    require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+    $APPLICATION->SetTitle("Главная");
 ?>
 
+
 <!-- ЭТО ВЫВОД СПИСКА НОВОСТЕЙ -->
-    <?
+<?php
     $APPLICATION->IncludeComponent(
 	"bitrix:news",
 	"",
     Array()
     );
-    ?>
+?>
 
-<?
+<?php
     require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
-    ?>
+?>
+
+
+
+
+</body>
+</html>
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -41,7 +41,6 @@ function parseInfo()
             }
         }
     }
-
     return "parseInfo();";
 }
 
